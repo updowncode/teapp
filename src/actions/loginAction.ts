25730d0987f5c8bc13d43fa45e3d4249094c7be1
updowncode <updowncode@gmail.com>
2018-04-18 09:07:00 -0400

    fetch get

--- src/actions/loginAction.ts
+++ src/actions/loginAction.ts
@@ -17,6 +17,18 @@ export const login = (user: any) => {
       Accept: 'application/json',
     };
     let HHname: any = { name: user.username };
+
+    fetch("https://b2b.toureast.com/api/heroes", {
+    method: "get"
+}).then(function(response) {
+    return response.text()
+}).then(function(data) {
+    console.log(data)
+    JSON.parse(data)
+}).catch(function(e) {
+  console.log("Oops, error");
+});
+
     // fetch(APIs.GETLIST, {
     //   method: 'GET',
     //   headers: requestHeaders,
@@ -24,6 +36,8 @@ export const login = (user: any) => {
     // }).then(
     //   response => {
     //     let resp = response.json();
+        
+        
     //     dispatch({
     //       type: ActionTypeKeys.LOGINSUCCESS,
     //       payload: {
@@ -41,20 +55,20 @@ export const login = (user: any) => {
     //   }
     // );
 
-    fetch('http://10.0.2.2:51796/api/heroes', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    }).then(
-      response => {
-        console.log('An error dfgdfg.');
-      },
-      error => {
-        console.log('An error occurred.', error);
-      }
-    );
+    // fetch('http://10.0.2.2:51796/api/heroes', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Accept: 'application/json',
+    //   },
+    // }).then(
+    //   response => {
+    //     console.log('An error dfgdfg.');
+    //   },
+    //   error => {
+    //     console.log('An error occurred.', error);
+    //   }
+    // );
     // setTimeout(() => {
     //   if (user.username == 'a' && user.password == 'a') {
     //     dispatch({
