1a2ca35491ea66d5a49fe0b0cfe45097421cfd28
updowncode <updowncode@gmail.com>
2018-05-16 17:48:55 -0400

    add swiper into tabnav

--- src/components/SplashComponent/SwiperGuideComponent.tsx
+++ src/components/SplashComponent/SwiperGuideComponent.tsx
@@ -1,41 +1,72 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Image, View, StyleSheet, Dimensions,Text, TouchableOpacity } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { NavigationScreenProp } from 'react-navigation';
+const image1 = require('../../../assets/images/guide1.png');
+const image2 = require('../../../assets/images/guide2.png');
+const image3 = require('../../../assets/images/guide3.png');
+const { width, height } = Dimensions.get('window');
 export interface IProps {
   navigation: NavigationScreenProp<any, any>;
 }
-
-export default class SwiperGuide extends Component<IProps, any> {
+export interface IStates {
+  swiperShow: boolean;
+}
+export default class SwiperGuide extends Component<IProps, IStates> {
   constructor(props) {
     super(props);
+    this.state = {
+      swiperShow: false,
+    };
   }
-  static navigationOptions = { title: 'Welcome', header: null };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        swiperShow: true,
+      });
+    }, 0);
+  }
+  static navigationOptions = { title: '', header: null };
   render() {
-    return (
-      <Swiper style={styles.wrapper} showsButtons={false} loop={false}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-          <View style={styles.container}>
-            <TouchableOpacity
-              style={styles.btn}
-              onPress={() => {
-                this.props.navigation.navigate('TabNavContainer');
-              }}
-            >
-              <Text style={styles.btnText}>启动应用</Text>
-            </TouchableOpacity>
+    if (this.state.swiperShow) {
+      return (
+        <Swiper
+          style={styles.wrapper}
+          showsButtons={false}
+          loop={false}
+          activeDotStyle={{ backgroundColor: '#ff7454', width: 10, height: 10 }}
+        >
+          <View style={styles.slide1}>
+            {/* <Text style={styles.text}>Hello</Text> */}
+
+            <Image source={image1} style={styles.backgroundImage} />
           </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>And simple</Text>
+            <View style={styles.container}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => {
+                  this.props.navigation.navigate('TabNavContainer');
+                }}
+              >
+                <Text style={styles.btnText}>启动应用</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Swiper>
+      );
+    } else {
+      return (
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Loading...</Text>
         </View>
-      </Swiper>
-    );
+      );
+    }
   }
 }
 const styles = StyleSheet.create({
@@ -48,6 +79,10 @@ const styles = StyleSheet.create({
     right: 0,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  backgroundImage: {
+    width: width,
+    height: height,
   },
   btn: {
     width: 150,
--- src/components/TabsComponents/AirTabComponent.tsx
+++ src/components/TabsComponents/AirTabComponent.tsx
@@ -1,20 +1,68 @@
 import * as React from 'react';
-import { View, StyleSheet, Text, } from 'react-native';
-
-export interface AppProps {
+import { Image, View, StyleSheet, Dimensions, Text } from 'react-native';
+import Swiper from 'react-native-swiper';
+const image1 = require('../../../assets/images/guide1.png');
+const image2 = require('../../../assets/images/guide2.png');
+const image3 = require('../../../assets/images/guide3.png');
+const { width, height } = Dimensions.get('window');
+export interface IProps {}
+export interface IStates {
+  swiperShow: boolean;
 }
+export default class ImgSwiper extends React.Component<IProps, IStates> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      swiperShow: false,
+    };
+  }
 
-export default class AppComponent extends React.Component<AppProps, any> {
-    constructor(props){
-        super(props);
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        swiperShow: true,
+      });
+    }, 100);
+  }
+
   render() {
-    return (
-      <View>
-         <Text>
-           Air
-         </Text>
-      </View>
-    );
+    if (this.state.swiperShow) {
+      return (<View style={styles.imgView}>
+        <Swiper
+          style={styles.imgWrapper}
+          showsButtons={false}
+          autoplay={false} //Flash when Remote Debugging.
+          loop={true}
+        >
+          <View style={styles.imgView}>
+            <Image source={image1} style={styles.bannerImg} />
+          </View>
+          <View style={styles.imgView}>
+            <Image source={image2} style={styles.bannerImg} />
+          </View>
+          <View style={styles.imgView}>
+            <Image source={image3} style={styles.bannerImg} />
+          </View>
+        </Swiper></View>
+      );
+    } else {
+      return (<Text>Loading...</Text>);
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  imgView: {
+    width:width,
+    height: 200,
+  },
+  imgWrapper: {
+    width: width,
+    height: 200,
+  },
+  bannerImg: {
+    width: width,
+    height: 200,
+    resizeMode: 'stretch',
+  },
+});
